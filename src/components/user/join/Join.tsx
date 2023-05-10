a980097f2a6364cdcc1a352264f4035df5343503
TaeWon Park <thisis_tw94@naver.com>
2023-05-10 22:54:20 +0900

    feat: 프로젝트 구조 설계

---
+++ src/components/user/join/Join.tsx
@@ -0,0 +1,9 @@
+function Join () {
+  return (
+    <>
+      Cart
+    </>
+  )
+}
+
+export default Join;
---
+++ src/components/user/myPage/MyPage.tsx
@@ -0,0 +1,9 @@
+function MyPage () {
+  return (
+    <>
+      Cart
+    </>
+  )
+}
+
+export default MyPage;
