12bde65e24418ba7616e8830d7beefca52129d22
TaeWon Park <thisis_tw94@naver.com>
2023-05-15 23:22:36 +0900

    feat: navBar 디자인 및 위치

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Bottom from "./components/home/Bottom";
-import * as styles from "./style/layout.css";
 
 
 function App() {
   return (
-    <div style={styles.app}>
+    <>
       <Outlet />
       <Bottom />
-    </div>
+    </>
   );
 }
 
