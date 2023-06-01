451b643047215429426c658df0ba0f6a0df3f359
TaeWon Park <thisis_tw94@naver.com>
2023-06-01 23:37:43 +0900

    feat: Input custom

--- src/components/user/join/PersonalInfo.tsx
+++ src/components/user/join/PersonalInfo.tsx
@@ -1,21 +1,51 @@
 import {Button, Form, Input} from "antd-mobile";
 import { useForm } from "react-hook-form";
+import StyledInput from "../../common/StyledInput";
 
 
 function PersonalInfo () {
+
+  // input common 컴포넌트로 만들기
 
   const { register } = useForm();
 
   return (
     <Form>
       <Form.Item name='howToEnter' label='출입방법' rules={[{ required: true }]}>
-        <Input {...register('howToEnter')} placeholder='출입방법을 입력해주세요' />
+        <StyledInput
+          {...register('howToEnter')}
+          type="text"
+          id="howToEnter"
+          placeholder="출입방법을 입력해주세요"
+          value=""
+          width="100%"
+          borderWidth="1px"
+          required={true}
+        />
       </Form.Item>
       <Form.Item name='requirementMemo' label='요청사항' rules={[{ required: true }]}>
-        <Input placeholder='요청사항을 입력해주세요' />
+        <StyledInput
+          {...register('requirementMemo')}
+          type="text"
+          id="requirementMemo"
+          placeholder="요청사항을 입력해주세요"
+          value=""
+          width="100%"
+          borderWidth="0.1px"
+          required={true}
+        />
       </Form.Item>
       <Form.Item name='memo' label='메모' rules={[{ required: true }]}>
-        <Input placeholder='메모를 입력해주세요' />
+        <StyledInput
+          {...register('memo')}
+          type="text"
+          id="memo"
+          placeholder="메모를 입력해주세요"
+          value=""
+          width="100%"
+          borderWidth= "0"
+          required={true}
+        />
       </Form.Item>
       <Button block color='primary' size='large' type="submit" onClick={() => console.log(register)}>
         가입하기
