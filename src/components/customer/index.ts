e010e251bd6d869a2d2ab5f247aeb14740eda1c4
TaeWon Park <thisis_tw94@naver.com>
2023-07-05 22:25:08 +0900

    chore : icon

--- src/components/customer/index.ts
+++ src/components/customer/index.ts
@@ -3,10 +3,25 @@ import NewCustomer from "../../global/interfaces/NewCustomer";
 
 const CustomerService = {
 
+  /* 참고항목:
+  * https://developers.kakao.com/docs/latest/ko/local/dev-guide#address-coord
+  * */
+
   create: (newCustomer: NewCustomer) => {
     return http
-      .post('/customers', newCustomer)
-  }
+      .get('https://dapi.kakao.com/v2/local/search/address.json', {
+        params: {query: newCustomer.address1},
+        headers: {
+          // 이렇게 하면 헤더 기존에  http 모듈에 세팅해놓은게 다 날라가서, 전개로 하는법 찾아야함.
+          Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_REST_KEY}`
+        }
+      })
+      .then((res) => console.log('확인---',res.data))
+      .then(() => {
+        http
+          .post('/customers', newCustomer)
+      })
+    }
 }
 
 export default CustomerService;
