45d5bb4520463c70313d9701a125ed6c3acf83a9
TaeWon Park <thisis_tw94@naver.com>
2023-07-20 21:49:51 +0900

    feat: login

--- src/components/customer/login/Login.tsx
+++ src/components/customer/login/Login.tsx
@@ -1,8 +1,37 @@
+import StyledLabel from "../../common/StyledLabel";
+import StyledInput from "../../common/StyledInput";
+import { BlockButton } from "../../common/StyledButton";
+
 function Login () {
+
+  const handleSubmit =(e: any) => {
+    e.preventDefault();
+    console.log('로그인!')
+  }
+
   return (
-    <>
-      login
-    </>
+    <form
+      onSubmit={handleSubmit}
+      style={{ display: "flex", flexDirection: "column", }}
+    >
+      <StyledLabel htmlFor='username'>아이디</StyledLabel>
+      <StyledInput
+        type='text'
+        id='username'
+        placeholder="아이디를 입력해주세요"
+        borderWidth="1px solid black"
+        width="100%"
+      />
+      <StyledLabel htmlFor='password'>비밀번호</StyledLabel>
+      <StyledInput
+        type='password'
+        id='password'
+        placeholder="비밀번호를 입력해주세요"
+        borderWidth="1px solid black"
+        width="100%"
+      />
+      <BlockButton>로그인</BlockButton>
+    </form>
   )
 }
 
